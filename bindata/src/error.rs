e1600c9a0d018e64ae142da090e67182ba40e8fe
htrefil <8711792+htrefil@users.noreply.github.com>
2020-06-06 10:38:47 +0200

    Implement Into<std::io::Error> for Error

--- bindata/src/error.rs
+++ bindata/src/error.rs
@@ -1,4 +1,5 @@
 use std::fmt::{self, Display, Formatter};
+use std::io::{self, ErrorKind};
 
 #[derive(Clone, Copy, Debug)]
 pub enum Error {
@@ -12,5 +13,11 @@ impl Display for Error {
             Error::Overflow => write!(f, "Out of bounds read"),
             Error::InvalidVariant => write!(f, "Invalid variant of an enum"),
         }
+    }
+}
+
+impl Into<io::Error> for Error {
+    fn into(self) -> io::Error {
+        io::Error::new(ErrorKind::InvalidData, self.to_string())
     }
 }
