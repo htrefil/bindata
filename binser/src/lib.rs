dd914b72c1707af83f34697dc8fd43ae72a149c4
htrefil <8711792+htrefil@users.noreply.github.com>
2020-06-06 11:28:01 +0200

    Rename to binser

--- binser/src/lib.rs
+++ binser/src/lib.rs
@@ -6,4 +6,4 @@ pub use decode::{Decode, Reader};
 pub use encode::{Encode, EncodedSize, Writer};
 pub use error::Error;
 
-pub use derive::{Decode, Encode, EncodedSize};
+pub use binser_derive::{Decode, Encode, EncodedSize};
